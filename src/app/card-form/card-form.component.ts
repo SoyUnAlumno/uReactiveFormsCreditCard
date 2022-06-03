b0bd1484d6d79eaed5e48b8db13a527c24aa3bcd
SoyUnAlumno <tomaspraga@hotmail.es>
2022-06-03 12:27:49 +0200

    validate user input without using components

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-form.component.css']
 })
 export class CardFormComponent implements OnInit {
+cardForm = new FormGroup({
+  name: new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ]),  
 
+});
   constructor() { }
 
   ngOnInit(): void {
